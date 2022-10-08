a5c9f3330d3ef08b718485791c3de66c82de36a8
evanmaiqa <mai.evan@yahoo.com>
2022-10-07 21:24:16 -0700

    upd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,16 +11,30 @@ public class Main {
         Employee employee_3 = new Employee("Wolfram", ' ', 1548, 1500);
 
         Employee[] employees = {employee_0, employee_1, employee_2, employee_3};
+//        Employee[] employees = new Employee[0];
+        if (SalaryUtils.isArrayEmptiness(employees)) {
+            System.out.println("No employees...");
+            System.exit(0);
+        }
+//        System.out.println(employee_0.isSameName(employee_0));
+//        System.out.println(employee_0.isSameName(employee_1));
 
-        System.out.println(employee_0.isSameName(employee_0));
-        System.out.println(employee_0.isSameName(employee_1));
+        // метод должен возвращать сумму зарплат
+        System.out.println(String.format("метод возвращат сумму зарплат_ %.2f", SalaryUtils.getSum(employees)));
 
-// метод должен возвращать сумму зарплат
-        System.out.println(SalaryUtils.getSum(employees));
-
-// поиск сотрудника в массиве по его имени
+        // поиск сотрудника в массиве по его имени
         SalaryUtils.isEmployeeExist(SalaryUtils.findEmployee("Ivan", employees));
 
+        // поиск сотрудника в массиве по вхождению указанной строки в его имени
+        System.out.println("по вхождению указанной строки_ " + SalaryUtils.findEmployeeByString("Ivaa", employees));
 
+        // подсчет зарплатного бюджета для всех сотрудников в массиве
+        System.out.println("бюджет : " + SalaryUtils.fullSalaryOfEmployees(employees));
+
+        // поиск наименьшей зарплаты в массиве
+        System.out.println("наименьшей зарплаты_ " + SalaryUtils.findMinSalary(employees));
+
+        // поиск наибольшей зарплаты в массиве
+        System.out.println("наибольшей зарплаты_ " + SalaryUtils.findMaxSalary(employees));
     }
 }
--- src/main/java/humans/SalaryUtils.java
+++ src/main/java/humans/SalaryUtils.java
@@ -8,7 +8,7 @@ package humans;
  * поиск наибольшей зарплаты в массиве
  */
 public class SalaryUtils {
-// метод должен возвращать сумму зарплат
+    // метод должен возвращать сумму зарплат
     public static double getSum(Employee[] employeeArray) {
         double sum = 0;
         for (Employee employee : employeeArray) {
@@ -16,7 +16,7 @@ public class SalaryUtils {
         }
         return sum;
     }
-// поиск сотрудника в массиве по его имени
+    // поиск сотрудника в массиве по его имени
     public static Employee findEmployee(String name, Employee[] employees) {
         for (Employee employee : employees) {
             if (employee.getName().equals(name)) {
@@ -27,13 +27,51 @@ public class SalaryUtils {
     }
     public static void isEmployeeExist (Employee employee) {
         if (employee == null) {
-            System.out.println("Employee not found");
+            System.out.println("по его имени_ Employee not found");
         } else {
-            System.out.println(String.format("%s %s %d %f", employee.getName(), employee.getSex(), employee.getAge(), employee.getSalary()));
+            System.out.println(String.format("по его имени_ %s %s %d %f", employee.getName(), employee.getSex(), employee.getAge(), employee.getSalary()));
         }
     }
 
-// поиск сотрудника в массиве по вхождению указанной строки в его имени
-
-// подсчет зарплатного бюджета для всех сотрудников в массиве
+    // поиск сотрудника в массиве по вхождению указанной строки в его имени
+    public static Employee findEmployeeByString (String name, Employee[] employees) {
+        for (Employee employee : employees) {
+            if (employee.getName().contains(name)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+    // подсчет зарплатного бюджета для всех сотрудников в массиве
+    public static double fullSalaryOfEmployees (Employee[] employees) {
+        double salary = 0;
+        for (Employee employee : employees) {
+            salary += employee.getSalary();
+        }
+        return salary;
+    }
+    // поиск наименьшей зарплаты в массиве
+    public static double findMaxSalary (Employee[] employees) {
+        double salary = Double.MIN_VALUE;
+        for (Employee employee : employees) {
+            if (employee.getSalary() > salary) {
+                salary = employee.getSalary();
+            }
+        }
+        return salary;
+    }
+    // поиск наибольшей зарплаты в массиве\
+    public static double findMinSalary (Employee[] employees) {
+        double salary = Double.MAX_VALUE;
+        for (Employee employee : employees) {
+            if (employee.getSalary() < salary) {
+                salary = employee.getSalary();
+            }
+        }
+        return salary;
+    }
+    // checking an array for emptiness
+    public static boolean isArrayEmptiness(Employee[] employees) {
+        return employees.length <= 0;
+    }
 }
